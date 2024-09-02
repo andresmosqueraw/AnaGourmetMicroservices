948b217fbfe79e1470f63d92824e659738c0d754
Andrew Mos <andresmosquera739@gmail.com>
2024-09-01 20:29:12 -0500

    Cors inventory & controller

--- microservice-inventory/src/main/java/com/microservice/inventory/controller/InventoryController.java
+++ microservice-inventory/src/main/java/com/microservice/inventory/controller/InventoryController.java
@@ -15,28 +15,33 @@ public class InventoryController {
     @Autowired
     private IInventoryService inventoryService;
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @PostMapping("/create")
     @ResponseStatus(HttpStatus.CREATED)
     public void saveInventory(@RequestBody Inventory inventory) {
         inventoryService.saveInventory(inventory);
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/all")
     public ResponseEntity<?> getAllInventories() {
         return ResponseEntity.ok(inventoryService.getAllInventories());
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/search/{id}")
     public ResponseEntity<?> findInventoryById(@PathVariable Long id) {
         return ResponseEntity.ok(inventoryService.getInventoryById(id));
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<?> deleteInventory(@PathVariable Long id) {
         inventoryService.deleteInventory(id);
         return ResponseEntity.ok("Inventory deleted");
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @PutMapping("/update/{id}")
     public ResponseEntity<?> updateInventory(@PathVariable Long id, @RequestBody Inventory inventory) {
         inventoryService.updateInventory(id, inventory);
--- microservice-sale/src/main/java/com/microservice/sale/controller/SaleController.java
+++ microservice-sale/src/main/java/com/microservice/sale/controller/SaleController.java
@@ -15,28 +15,33 @@ public class SaleController {
     @Autowired
     private ISaleService saleService;
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @PostMapping("/create")
     @ResponseStatus(HttpStatus.CREATED)
     public void saveSale(@RequestBody Sale sale) {
         saleService.saveSale(sale);
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/all")
     public ResponseEntity<?> getAllSales() {
         return ResponseEntity.ok(saleService.getAllSales());
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/search/{id}")
     public ResponseEntity<?> findSaleById(@PathVariable Long id) {
         return ResponseEntity.ok(saleService.getSaleById(id));
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<?> deleteSale(@PathVariable Long id) {
         saleService.deleteSale(id);
         return ResponseEntity.ok("Sale deleted");
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @PutMapping("/update/{id}")
     public ResponseEntity<?> updateSale(@PathVariable Long id, @RequestBody Sale sale) {
         saleService.updateSale(id, sale);
