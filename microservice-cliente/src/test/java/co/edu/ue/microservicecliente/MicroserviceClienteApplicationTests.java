d3be930765b3dc0a28ae334e681e8ec975621148
Andres Felipe Nieto Jimenez <81382324+FitzPitx@users.noreply.github.com>
2024-09-14 22:34:34 -0500

    new microservice

---
+++ microservice-cliente/src/test/java/co/edu/ue/microservicecliente/MicroserviceClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.ue.microservicecliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
